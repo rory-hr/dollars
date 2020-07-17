36de506409d38a4300d90c12847bc9d0f521002d
rory-roccio <rory.roccio@gmail.com>
2020-07-17 14:26:33 -0700

    Create component

---
+++ src/screens/toptabs/Dash/Dash.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { useSelector } from 'react-redux';
+import { useTheme } from '@react-navigation/native';
+
+
+// Components
+import Main from './Main';
+import Expenses from './Expenses';
+
+export default () => {
+
+  // Style
+  const { colors } = useTheme();
+
+  const styles = {
+    container: {
+      flex: 1,
+      flexDirection: 'column',
+      backgroundColor: 'white',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+    }
+  };
+
+  // Data
+  const budget = useSelector(state => state.budget);
+
+  return (
+    <View style={styles.container}>
+      <Main />
+      <Expenses />
+    </View>
+  );
+};
