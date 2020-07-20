eee921afa170360219e2c8ffe53648f6b5fda64b
rory-roccio <rory.roccio@gmail.com>
2020-07-20 15:04:04 -0700

    Put data processing logic here, change child component names and imports

--- src/screens/toptabs/Dash/Dash.jsx
+++ src/screens/toptabs/Dash/Dash.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { useTheme } from '@react-navigation/native';
+import moment from 'moment';
+import * as utils from '../../../utils/calculations';
 
 
 // Components
-import Main from './Main';
-import Expenses from './Expenses';
+import Overview from './Overview';
+import Table from './Table';
 
 export default () => {
+
+  // Data
+  const now = moment();
+  const data = useSelector(state => state.data);
+  const currentYear = data[data.length - 1];
+  const currentMonth = currentYear.months[parseInt(now.format('M')) - 1];
+  const netAllowance = utils.netAllowance(currentMonth, parseInt(moment().format('D')));
+  const today = moment().format('MMMM D');
+  const monthlyAllowance = utils.dailyAllowance(currentMonth);
+  const todaysDate = parseInt(moment().format('D'));
+  const currentDay = currentMonth.days[todaysDate - 1];
+  const todaysExpenses = utils.sumExpenses(currentDay);
 
   // Style
   const { colors } = useTheme();
@@ -23,12 +37,10 @@ export default () => {
     }
   };
 
-  
-
   return (
     <View style={styles.container}>
-      <Main />
-      <Expenses />
+      <Overview netAllowance={netAllowance} today={today} monthlyAllowance={monthlyAllowance} todaysExpenses={todaysExpenses}/>
+      <Table currentDay={currentDay}/>
     </View>
   );
 };
